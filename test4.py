5cda889b154095c26de9444b3878461adf79400e
TAGEMAN <r3answer@163.com>
2018-02-27 18:00:12 +0800

    math.sqrt and math.floor

---
+++ test4.py
@@ -0,0 +1,29 @@
+# -*- coding=UTF-8 -*-
+"""
+You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the parameter is itself not a perfect square, than -1 should be returned. You may assume the parameter is positive.
+
+Examples:
+
+findNextSquare(121) --> returns 144
+findNextSquare(625) --> returns 676
+findNextSquare(114) --> returns -1 since 114 is not a perfect
+
+"""
+
+
+
+from math import sqrt,floor
+
+
+def find_next_square(sq):
+    if sqrt(sq)-floor(sqrt(sq)) == 0:
+        return int((sqrt(sq)+1)**2)
+    return -1
+
+
+if __name__ == '__main__':
+    find_next_square(121)
